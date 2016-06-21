d9bb39269852e9a3cf34d36617f6db991ed7d495
Oscar Roa <oshardy3@gmail.com>
2016-06-21 03:08:48 -0500

    Add quizzes app

---
+++ quizzes/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from cultures.models import Culture
+
+
+class Question(models.Model):
+
+    culture = models.ForeignKey(Culture)
+    question = models.CharField(max_length=500)
+
+    # Metadata
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Question: {question}".format(self.question)
+
+
+class Answer(models.Model):
+
+    question = models.ForeignKey(Question)
+    answer = models.CharField(max_length=250)
+
+    is_correct = models.BooleanField(default=False)
+
+    # Metadata
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{answer} from {question}".format(
+            answer=self.answer,
+            question=self.question
+        )
