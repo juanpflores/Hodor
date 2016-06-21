67045e833325251979aa7b84da37096c3b6895d8
Oscar Roa <oshardy3@gmail.com>
2016-06-20 22:49:02 -0500

    Urls

--- cultures/views.py
+++ cultures/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from django.views.generic import(
+    ListView,
+    DetailView,
+    TemplateView
+)
+
+from .models import (
+    Language,
+    Religion,
+    Culture,
+    God,
+    Temple,
+    Museum
+)
+
+
+class LandingView(ListView):
+    template_name = 'prueba.html'
+    model = Culture
+
+
+class AdventureView(DetailView):
+    template_name = 'landing-maya.html'
+    model = Culture
+
+    def get_object(self):
+        culture = get_object_or_404(Culture, name=self.kwargs['name'])
+        return culture
